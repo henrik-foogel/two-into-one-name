0d6c5bdf9ac9435ecc22dbf49bce8a74445e6b10
Henrik Foogel <henrik.foogel@medieinstitutet.se>
2019-01-31 15:45:13 +0100

    first commit

---
+++ labserver.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+const http = require('http');
+
+var options = {
+    encoding: 'utf8',
+    flag: 'r'
+};
+var optionsTwo = {encoding: 'utf8', flag: 'w'};
+
+var firstFile = fs.readFile('./one.txt', options, (err, data) => {
+    if(err) {
+        console.log(err)
+    } else {
+        console.log(data)
+    }
+    var secondFile = fs.readFile('./two.txt', options, (err, data2) => {
+        if(err) {
+            console.log(err)
+        } else {
+            console.log(data2);
+        }
+        fs.writeFile('third.txt', data+data2, optionsTwo, (err) => {
+            if(err) {
+                console.log(err)
+            } else {
+                console.log('Filen är sparad')
+            }
+
+            var thirdFile = fs.readFile('./third.txt', options, (err, data3) => {
+                if(err) {
+                    console.log(err)
+                } else {
+                    console.log(data3);
+                }
+                const webbserver = http.createServer((req, res) => {
+                        res.write(data3)
+                        res.end();
+                });
+                
+                webbserver.listen(3000, () => {
+                    console.log('Server is up on 3000')
+                });
+            })
+        });
+    })
+});
+
+
+
